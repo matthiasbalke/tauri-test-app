d5e30327d1278a57b46762be6b0a3202163f2be0
Matthias Balke <Matthias.Balke@googlemail.com>
2026-03-01 02:38:35 +0100

    add some update logging

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,7 +15,9 @@ pub fn run() {
         .setup(|app| {
             let handle = app.handle().clone();
             tauri::async_runtime::spawn(async move {
-                update(handle).await.unwrap();
+                if let Err(err) = update(handle).await {
+                    eprintln!("Failed to check for updates: {}", err);
+                }
             });
             Ok(())
         })
@@ -24,8 +26,18 @@ pub fn run() {
 }
 
 async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
+    println!("checking for updates ...");
     if let Some(update) = app.updater()?.check().await? {
         let mut downloaded = 0;
+
+        println!(
+            "updating to version {} released on {}",
+            update.version,
+            update
+                .date
+                .map(|d| d.to_string())
+                .unwrap_or_else(|| String::from("<unknown date>"))
+        );
 
         // alternatively we could also call update.download() and update.install() separately
         update
@@ -42,6 +54,8 @@ async fn update(app: tauri::AppHandle) -> tauri_plugin_updater::Result<()> {
 
         println!("update installed");
         app.restart();
+    } else {
+        println!("no update found.");
     }
 
     Ok(())
